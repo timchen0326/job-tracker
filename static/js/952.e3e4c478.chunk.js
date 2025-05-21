22e5a3a9d15accba83e2db26a0bdeaf5d06197ca
Tim Chen <timchen0326ca@gmail.com>
2025-05-20 21:42:44 -0400

    Updates

---
+++ static/js/952.e3e4c478.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjob_tracker=self.webpackChunkjob_tracker||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.e3e4c478.chunk.js.map
